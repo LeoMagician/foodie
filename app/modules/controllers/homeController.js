03dd55a8164a85cc4a550e152008d34d0787380e
LeoMagician <leo.magician11@gmail.com>
2016-08-15 10:25:32 -0400

    homepage

--- app/modules/controllers/homeController.js
+++ app/modules/controllers/homeController.js
@@ -5,11 +5,25 @@ define(function() {
             $scope.name = "Home";
             $scope.test = "hello there";
         })
-        .directive("headerTemp", function() {
+        .directive("homePic", function() {
             return {
                 restrict: 'AC',
-                templateUrl: '/app/modules/views/templates/headerTemp.html',
+                templateUrl: '/app/modules/views/templates/home/homePic.html',
                 replace: true
+            }
+        })
+        .directive("homeTitle", function() {
+            return {
+                restrict: 'AC',
+                templateUrl: '/app/modules/views/templates/home/homeTitle.html',
+                replace: false
+            }
+        })
+        .directive("homeContent", function() {
+            return {
+                restrict: 'AC',
+                templateUrl: '/app/modules/views/templates/home/homeContent.html',
+                replace: false
             }
         });
 });
