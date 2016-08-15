36d1afa08ce6f708fae1d1df7a212d703f887a70
Xuejia(Leo) Li <leo.li@everbridge.com>
2016-08-14 23:46:59 -0400

    homepage

--- app/modules/controllers/homeController.js
+++ app/modules/controllers/homeController.js
@@ -2,7 +2,14 @@ define(function() {
     var coreModule = angular.module('coreModule');
 
     coreModule.controller('homeController', function($scope) {
-        $scope.name = "Home";
-        $scope.test = "hello there";
-    });
+            $scope.name = "Home";
+            $scope.test = "hello there";
+        })
+        .directive("headerTemp", function() {
+            return {
+                restrict: 'AC',
+                templateUrl: '/app/modules/views/templates/headerTemp.html',
+                replace: true
+            }
+        });
 });
