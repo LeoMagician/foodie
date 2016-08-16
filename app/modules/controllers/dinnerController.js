d40c91598b912ffe14194e903d7bf3f7a00d22c3
xingshu625 <xingshucui@gmail.com>
2016-08-15 22:59:36 -0400

    new one

--- app/modules/controllers/dinnerController.js
+++ app/modules/controllers/dinnerController.js
@@ -3,5 +3,26 @@ define(function() {
 
     coreModule.controller('dinnerController', function($scope) {
         $scope.name = "dinner page";
-    });
+    })
+    .directive("bookTemp", function() {
+            return {
+                restrict: 'AC',
+                templateUrl: '/app/modules/views/templates/dinner/bookTemp.html',
+                replace: false
+            }
+        })
+    .directive("hostinfoTemp", function() {
+            return {
+                restrict: 'AC',
+                templateUrl: '/app/modules/views/templates/dinner/hostinfoTemp.html',
+                replace: false
+            }
+        })
+       .directive("reviewTemp", function() {
+            return {
+                restrict: 'AC',
+                templateUrl: '/app/modules/views/templates/dinner/reviewTemp.html',
+                replace: false
+            }
+        }) 
 });
