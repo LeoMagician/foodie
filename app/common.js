4b1efae8080601a501d4dbf965ff04ecd5e275ae
xingshu625 <xingshucui@gmail.com>
2016-08-15 23:07:10 -0400

    'lo'

--- app/common.js
+++ app/common.js
@@ -4,6 +4,9 @@ requirejs.config({
         'angular-route': '/bower_components/angular-route/angular-route.min',
         'jquery': '/bower_components/jquery/dist/jquery.min',
         'ui-bootstrap': '/bower_components/angular-bootstrap/ui-bootstrap-tpls.min',
+        // 'async': 'bower_components/requirejs-plugins/src/async',
+        // 'google-maps': 'bower_components/angular-google-maps/dist/angular-google-maps',
+        // 'ngMap': '/bower_components/ngmap/build/scripts/ng-map.min',
         //load the modules
         'coreModule': '/app/modules/coreModule'
     },
