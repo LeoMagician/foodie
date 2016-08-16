64b99faa79d7507c9b0b386fe18a094493f47d62
Jianing Wang <nekoningwang@gmail.com>
2016-08-15 23:17:55 -0400

    lol

--- app/modules/controllers/dinnerController.js
+++ app/modules/controllers/dinnerController.js
@@ -3,5 +3,30 @@ define(function() {
 
     coreModule.controller('dinnerController', function($scope) {
         $scope.name = "dinner page";
-    });
+    })
+
+    .directive("pictureTemp", function() {
+        return {
+            restrict: 'AC',
+            templateUrl: '/app/modules/views/templates/dinner/pictureTemp.html',
+            replace: false
+            }
+    })
+
+    .directive("personInfoTemp", function() {
+        return {
+            restrict: 'AC',
+            templateUrl: '/app/modules/views/templates/dinner/personInfoTemp.html',
+            replace: false
+            }
+    })
+
+    .directive("descriptionTemp", function() {
+        return {
+            restrict: 'AC',
+            templateUrl: '/app/modules/views/templates/dinner/descriptionTemp.html',
+            replace: false
+            }
+    })
 });
+
