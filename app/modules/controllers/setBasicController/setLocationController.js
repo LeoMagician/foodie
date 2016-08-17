e57a4e57f2786a55f6a4aa1aa85a954d22dd84a7
LeoMagician <leo.magician11@gmail.com>
2016-08-17 10:48:14 -0400

    new part

---
+++ app/modules/controllers/setBasicController/setLocationController.js
@@ -0,0 +1,7 @@
+define(function() {
+    var coreModule = angular.module('coreModule');
+
+    coreModule.controller('setLocationController', function($scope) {
+        $scope.name = "setLocationController";
+    });
+});
