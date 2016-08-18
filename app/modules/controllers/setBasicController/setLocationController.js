ada9a325d0d60e862f5612f0fe8c215b2972d82c
Jianing Wang <nekoningwang@gmail.com>
2016-08-18 00:47:41 -0400

    up

--- app/modules/controllers/setBasicController/setLocationController.js
+++ app/modules/controllers/setBasicController/setLocationController.js
@@ -4,4 +4,5 @@ define(function() {
     coreModule.controller('setLocationController', function($scope) {
         $scope.name = "setLocationController";
     });
+
 });
---
+++ app/modules/controllers/setBasicController/setPlaceTypeController.js
@@ -0,0 +1,8 @@
+define(function() {
+    var coreModule = angular.module('coreModule');
+
+    coreModule.controller('setPlaceTypeController', function($scope) {
+        $scope.name = "setPlaceTypeController";
+    });
+
+});
